8b2a217287a272b2968eeeafb140609c272e71d8
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-14 19:45:32 +0200

    Add analyzeArray function

--- code/analyzeArray.js
+++ code/analyzeArray.js
@@ -1,10 +1,12 @@
 const analyzeArray = (arr) => {
+    
     return {
-        average: 4,
-        min: 1,
-        max: 8,
-        length: 6
-      }
+        average: arr.reduce((acc,curr) => acc + curr, 0) / arr.length,
+        min: Math.min(...arr),
+        max: Math.max(...arr),
+        length: arr.length
+    }
+     
 }
 
 module.exports = analyzeArray;
--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,6 +1,15 @@
 const analyzeArray = require('../code/analyzeArray');
 
-test('Capitalizes single word sentences', () => {
+test('Analyzes array with two elements', () => {
+    expect(analyzeArray([4,11])).toEqual({
+        average: 7.5,
+        min: 4,
+        max: 11,
+        length: 2
+      });
+  });
+
+test('Analyzes longer array', () => {
     expect(analyzeArray([1,8,3,4,2,6])).toEqual({
         average: 4,
         min: 1,
