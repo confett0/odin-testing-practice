7ef165696e56d46df0db6e09c38f7645c6a3bf31
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-14 17:26:54 +0200

    Add more reverseString tests

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -3,4 +3,16 @@ const reverseString = require('../code/reverseString');
 test('Reverses single word sentences', () => {
     expect(reverseString("hello")).toBe("olleh");
   });
+
+  test('Returns "" with an empty string', () => {
+    expect(reverseString("")).toBe("");
+  });
+
+  test('Reverses multiple word sentences', () => {
+    expect(reverseString("hello world")).toBe("dlrow olleh");
+  });
+
+  test('Mantains capitalizations', () => {
+    expect(reverseString("Hello World")).toBe("dlroW olleH");
+  });
   
