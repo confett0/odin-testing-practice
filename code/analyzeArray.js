3f7d73281833950a622b38c8b3d9a7f4b1186213
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-14 19:39:51 +0200

    Add analyzeArray first test

--- code/analyzeArray.js
+++ code/analyzeArray.js
@@ -1,3 +1,10 @@
-const analyzeArray = (arr) => {}
+const analyzeArray = (arr) => {
+    return {
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+      }
+}
 
 module.exports = analyzeArray;
